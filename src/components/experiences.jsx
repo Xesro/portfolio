8dd61df2cad0c37ff681bfd5d835f3e253d2eccb
Arthur Plebs <arthur.45@live.fr>
2024-12-17 21:32:41 +0100

    add arrow experience and hover on scroll for mobile

--- src/components/experiences.jsx
+++ src/components/experiences.jsx
@@ -1,20 +1,45 @@
+"use client"
+
 import {LineSection} from "@/components/ui/line-section";
 import {BentoCard, BentoGrid} from "@/components/magicui/bento-grid";
-import { Droplet, Book, Truck, Video} from "lucide-react";
+import { Droplet, Book, Truck, Video, CloudCog} from "lucide-react";
 import Image from "next/image";
 import {cn} from "@/lib/utils";
 
 
 const experiences = [
     {
+        Icon: CloudCog,
+        name: "Arrow Sphere - Freelance Cloud Engineer on AWS",
+        description: "Migrating a Laravel monolith to a serverless architecture for scalability purposes",
+        className: "col-span-5 lg:col-span-3",
+        background: (
+            <Image
+                src={'/arrow_bg.webp'}
+                alt={"arrow"}
+                width={0}
+                height={0}
+                style={{width: "100%", height: "auto"}}
+                sizes="100vw"/>
+        ),
+        content: (
+            <ul className={"list-disc"}>
+                <li>Reimplement key features using AWS lambda, Event Bridge and Step function</li>
+                <li>Audit legacy monolith to implement features seamlessly</li>
+                <li>Continuous deployement with Jenkins and Cloud Formation</li>
+                <li>Unit test</li>
+            </ul>
+        )
+    },
+    {
         Icon: Truck,
-        name: "Ownest",
+        name: "Ownest - Freelance Blockchain Engineer",
         description: "Responsible for the evolution of the NFT platform API",
-        className: "col-span-3 lg:col-span-2",
+        className: "col-span-5 lg:col-span-2",
         background: (
             <Image
                 src={'/ownest_bg.png'}
-                alt={"lilo"}
+                alt={"ownest"}
                 width={0}
                 height={0}
                 style={{width: "100%", height: "auto"}}
@@ -23,40 +48,18 @@ const experiences = [
         content: (
             <ul className={"list-disc"}>
                 <li>Lead the developement of the NFT platform API built on Nestjs and MongoDB.</li>
+                <li>Smart contracts development & blockchain interactions</li>
                 <li>Maintain CI/CD pipelines</li>
                 <li>Security reviews</li>
                 <li>Evolution of GCP infrastructure</li>
-                <li>Smart contracts development & blockchain interactions</li>
-            </ul>
-        )
-    },
-    {
-        Icon: Book,
-        name: "Learnz",
-        description: "Co-founder of the learning platform powered by AI",
-        className: "col-span-3 lg:col-span-1",
-        background: (
-            <Image
-                src={'/learnz_bg.jpeg'}
-                alt={"lilo"}
-                width={0}
-                height={0}
-                style={{width: "100%", height: "auto"}}
-                sizes="100vw"/>
-        ),
-        content: (
-            <ul className={"list-disc"}>
-                <li>R&D on closed and open source LLM models to optimise results and costs</li>
-                <li>Build and maintain the whole infrastructure orchestrated with Docker Swarm</li>
-                <li>Business development</li>
             </ul>
         )
     },
     {
         Icon: Video,
-        name: "Tiktools",
-        description: "Co-founder of an automation tool for social media content creators",
-        className: "col-span-3 lg:col-span-1",
+        name: "Tiktools - Co-founder - Sold",
+        description: "Automation tool for social media content creators",
+        className: "col-span-5 lg:col-span-2",
         background: (
             <Image
                 src={'/tiktools_bg.png'}
@@ -70,14 +73,16 @@ const experiences = [
             <ul className={"list-disc"}>
                 <li>Business development</li>
                 <li>Build an application that can handle video creation workloads and IA process using Python for workers ans Nestjs app for the main backend</li>
+                <li>Ci/CD pipeline</li>
+                <li>Manage a Docker Swarm infrastructure hosted a VPS</li>
             </ul>
         )
     },
     {
         Icon: Droplet,
-        name: "Lilo",
-        description: "Backend engineer",
-        className: "col-span-3 lg:col-span-2",
+        name: "Lilo - Backend engineer",
+        description: "Socially responsible Search Engine",
+        className: "col-span-5 lg:col-span-3",
         background: (
             <Image
                 src={'/lilo_bg.png'}
--- src/components/magicui/bento-grid.jsx
+++ src/components/magicui/bento-grid.jsx
@@ -1,58 +1,104 @@
-import { ArrowRightIcon } from "@radix-ui/react-icons";
+"use client"
 
+import { useState, useEffect, useRef } from 'react';
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 
 const BentoGrid = ({
-  children,
-  className
-}) => {
-  return (
-    (<div
-      className={cn("grid w-full auto-rows-[22rem] grid-cols-3 gap-4", className)}>
-      {children}
-    </div>)
-  );
+                       children,
+                       className
+                   }) => {
+    return (
+        <div
+            className={cn("grid w-full auto-rows-[22rem] grid-cols-5 gap-4", className)}>
+            {children}
+        </div>
+    );
 };
 
 const BentoCard = ({
-  name,
-  className,
-  background,
-  Icon,
-  description,
-    content
-}) => (
-    <div
-        key={name}
-        className={cn(
-            "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
-            // light styles
-            "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
-            // dark styles
-            "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
-            "h-[350px]",
-            className
-        )}>
-        <div className={"w-full h-full absolute"}>
-            {background}
-        </div>
-        <div className={"absolute  h-full w-full"}></div>
-        <div
-            className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-[175px] absolute top-[190px] bg-gradient-to-t from-gray-50  from-90% to-transparent min-h-[350px] w-full">
-            <div className={"h-[125px]"}>
-                <Icon
-                    className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75"/>
-                <h3 className="text-xl font-semibold text-neutral-800 ">
-                    {name}
-                </h3>
-                <p className="max-w-lg text-neutral-700">{description}</p>
-            </div>
-            <div className={"text-neutral-600 p-6"}>
-                {content}
-            </div>
-        </div>
-    </div>
-);
+                       name,
+                       className,
+                       background,
+                       Icon,
+                       description,
+                       content
+                   }) => {
+    const [isMobileHovered, setIsMobileHovered] = useState(false);
+    const cardRef = useRef(null);
 
-export {BentoCard, BentoGrid};
+    useEffect(() => {
+        const handleIntersection = (entries) => {
+            console.log(entries)
+            entries.forEach(entry => {
+                if (entry.isIntersecting && window.innerWidth <= 768) {
+                    setIsMobileHovered(true);
+                } else {
+                    setIsMobileHovered(false)
+                }
+            });
+        };
+
+        const observer = new IntersectionObserver(handleIntersection, {
+            threshold: 1
+        });
+
+        if (cardRef.current) {
+            observer.observe(cardRef.current);
+        }
+
+        return () => {
+            if (cardRef.current) {
+                observer.unobserve(cardRef.current);
+            }
+        };
+    }, []);
+
+    return (
+        <div
+            ref={cardRef}
+            key={name}
+            className={cn(
+                "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
+                // light styles
+                "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+                // dark styles
+                "transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+                "h-[350px]",
+                className
+            )}>
+            <div className={"w-full h-full absolute bg-white"}>
+                {background}
+            </div>
+            <div className={"absolute h-full w-full"}></div>
+            <div
+                className={cn(
+                    "pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 absolute top-[170px] bg-gradient-to-t from-gray-50 from-90% to-transparent min-h-[350px] w-full",
+                    "group-hover:-translate-y-[175px]",
+                    {
+                        "-translate-y-[175px]": isMobileHovered
+                    }
+                )}>
+                <div className={""}>
+                    <Icon
+                        className={cn(
+                            "h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out",
+                            "group-hover:scale-75",
+                            {
+                                "scale-75": isMobileHovered
+                            }
+                        )}
+                    />
+                    <h3 className="text-md lg:text-xl font-semibold text-neutral-800 ">
+                        {name}
+                    </h3>
+                    <p className="text-xs md:text-base max-w-lg text-neutral-700">{description}</p>
+                </div>
+                <div className={"text-xs md:text-base text-neutral-600 p-6"}>
+                    {content}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export { BentoCard, BentoGrid };
